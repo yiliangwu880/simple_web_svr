938590fc6cee77dfdd771e301fa33f8fce87f84e
yiliangwu <30390012@qq.com>
2021-02-05 17:36:39 +0800

    m

--- src/AppMgr.cpp
+++ src/AppMgr.cpp
@@ -21,17 +21,14 @@ public:
 void AppMgr::Start(int argc, char* argv[])
 {
 	TriggerEvent<AE_CFG_INI>(); //配置初始化
-
 	SingleProgress::Ins().Check(argc, argv, "web_svr", AppMgr::Ins().m_isDaemon); //启动关闭进程管理
 	SuMgr::Ins().Init();
-
 	EventMgr::Ins().Init(&MyLcLog::Ins());
-	L_INFO("after EventMgr::Ins().Init ");
-	TriggerEvent<AE_AFTER_NET_INT>();
 	lc::Timer timer; //一个进程只需要一个lc::Timer，其他定时器由  svr_util 驱动
 	auto f = std::bind(&AppMgr::OnTimer, this);
 	timer.StartTimer(30, f, true);
-	
+
+	TriggerEvent<AE_AFTER_NET_INT>();
 	EventMgr::Ins().Dispatch();
 	L_INFO("main end");
 }
